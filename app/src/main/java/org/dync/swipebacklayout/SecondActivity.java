7d3461c9501db8c1068383ce629812ecde385aa1
DyncKathline <1350624667@qq.com>
2016-09-06 14:56:55 +0800

    添加viewpager样例

--- app/src/main/java/org/dync/swipebacklayout/SecondActivity.java
+++ app/src/main/java/org/dync/swipebacklayout/SecondActivity.java
@@ -1,12 +1,41 @@
 package org.dync.swipebacklayout;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecondActivity extends SwipeBackActivity {
+    private ViewPager viewPager;
+    private int []imageIdArray;//图片资源的数组
+    private List<View> viewList;//图片资源的集合
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+
+        imageIdArray = new int[]{R.drawable.a, R.drawable.b, R.drawable.c, R.drawable.d};
+        viewList = new ArrayList<>();
+        //获取一个Layout参数，设置为全屏
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.MATCH_PARENT);
+        //循环创建View并加入到集合中
+        int len = imageIdArray.length;
+        for (int i = 0;i<len;i++){
+            //new ImageView并设置全屏和图片资源
+            ImageView imageView = new ImageView(this);
+            imageView.setLayoutParams(params);
+            imageView.setBackgroundResource(imageIdArray[i]);
+
+            //将ImageView加入到集合中
+            viewList.add(imageView);
+        }
+        viewPager.setAdapter(new ViewPagerAdapter(viewList));
     }
 }
